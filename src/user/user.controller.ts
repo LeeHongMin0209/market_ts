3b67c387c4bda5e55da58629bc039f9ae2ad869d
LeeHongMin <tmxkzm35@naver.com>
2020-11-02 19:33:51 +0900

    회원정보수정 api 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './schemas/user.schema';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 
@@ -9,13 +10,21 @@ import { User } from './schemas/user.schema';
 export class UserController {
     constructor(private readonly userService: UserService) {}
 
+    // Post -> 유저 회원가입
     @Post()
     async create(@Body() createUserDto: CreateUserDto){
         return this.userService.createUser(createUserDto);
     }
 
+    // Get -> 이메일 찾기
     @Get('/findEmail')
     async findEmail(@Body() params){
         return this.userService.findEmail(params);
+    }
+
+    //Put -> 회원 정보 수정하기
+    @Put('/changeUserData')
+    async changeUserData(@Body() updateUserDto: UpdateUserDto){
+        return this.userService.changeUserData(updateUserDto);
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,7 +2,7 @@ import { Model } from 'mongoose';
 import { HttpException, HttpStatus, Inject, Injectable, Post } from '@nestjs/common';
 import { User, UserDocument } from './schemas/user.schema';
 import { CreateUserDto } from './dto/create-user.dto';
-
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
@@ -11,6 +11,7 @@ export class UserService {
         private userModel: Model<UserDocument>,
       ) {}
 
+    // Post -> 유저 회원가입
     async createUser(createUserDto: CreateUserDto) {
         const emailUser: UserDocument | null = await this.userModel.findOne({ email: createUserDto.email });
         const phoneUser: UserDocument | null = await this.userModel.findOne({ phone: createUserDto.phone });
@@ -27,6 +28,7 @@ export class UserService {
       }
     }
 
+    // Get -> 이메일 찾기
     async findEmail(params): Promise<User> {
       const { name, phone } = params
       const user: UserDocument | null = await this.userModel.findOne({ name, phone }).select('_id email');
@@ -35,6 +37,21 @@ export class UserService {
         return user;
       } else {
         throw new HttpException('아이디를 찾지 못했습니다.', HttpStatus.FORBIDDEN)
+      }
+    }
+
+    // Put -> 회원 정보 수정하기
+    async changeUserData(updateUserDto: UpdateUserDto) :Promise<User> {
+      const userId: string = updateUserDto._id;
+      const user: UserDocument | null = await this.userModel.findById(userId);
+      if(user){
+        user.email = updateUserDto.email;
+        user.name = updateUserDto.name;
+        user.phone = updateUserDto.phone;
+        user.agreeMarketingInfo = updateUserDto.agreeMarketingInfo;
+        return user.save();
+      } else {
+        throw new HttpException('해당 id의 회원정보가 존재하지 않습니다.', HttpStatus.FORBIDDEN)
       }
     }
 }
